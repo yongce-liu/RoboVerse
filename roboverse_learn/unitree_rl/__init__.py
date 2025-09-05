eb982c1b36b5f55ee9e5ab86d2b4584ccf149338
myuansun <1273414643@qq.com>
2025-09-05 20:47:22 +0800

    add g1_dof29 robot

--- roboverse_learn/unitree_rl/__init__.py
+++ roboverse_learn/unitree_rl/__init__.py
@@ -2,3 +2,4 @@
 from .tasks.dof12_walking import Dof12WalkingCfg, Dof12WalkingTask
 from .tasks.humanoid_walking import HumanoidWalkingCfg, HumanoidWalkingTask
 from .tasks.legged_walking import LeggedWalkingCfg, LeggedWalkingTask
+from .tasks.g1_dof29_walking import G1Dof29WalkingCfg, G1Dof29WalkingTask
