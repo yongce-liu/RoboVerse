b819e2cf9a5270ea533c0cc3d7fa08bc6d6cecb5
yongce-liu <airrighttime@outlook.com>
2025-09-18 07:11:05 +0000

    update

--- roboverse_learn/rl/unitree_rl/configs/base_legged.py
+++ roboverse_learn/rl/unitree_rl/configs/base_legged.py
@@ -174,7 +174,7 @@ class LeggedRobotDomainRandCfg:
 
     push = PushRandomCfg(enabled=True)
 
-    terrain_cfg: GroundCfg = GroundCfg.from_yaml(pathlib.Path(__file__).parent / "terrain.yaml")
+    terrain_cfg: GroundCfg = GroundCfg.from_yaml(pathlib.Path(__file__).parent / "tmp_terrain.yaml")
     """Terrain randomization configuration"""
 
     def __post_init__(self):
