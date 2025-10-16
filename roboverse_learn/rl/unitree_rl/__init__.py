7557647ae20579145e3e751f2f45241643085ce0
myuansun <1273414643@qq.com>
2025-10-16 18:04:34 +0800

    delete legged_walking

--- roboverse_learn/rl/unitree_rl/__init__.py
+++ roboverse_learn/rl/unitree_rl/__init__.py
@@ -1,4 +1,3 @@
 # ruff: noqa: F401
 from .tasks.humanoid_walking import HumanoidWalkingCfg, HumanoidWalkingTask
-from .tasks.legged_walking import LeggedWalkingCfg, LeggedWalkingTask
 from .tasks.g1_dof29_walking import G1Dof29WalkingCfg, G1Dof29WalkingTask
