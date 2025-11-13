8813603d51a0ee83d608ad25bd733237fbf45b31
MurphyZhao04 <xh930717304@gmail.com>
2025-11-13 10:40:21 -0800

    [fix] robot cfg import

--- roboverse_pack/robots/__init__.py
+++ roboverse_pack/robots/__init__.py
@@ -16,6 +16,7 @@ from .franka_urdf_sapien_cfg import FrankaUrdfSapienCfg
 from .franka_with_gripper_extension_cfg import FrankaWithGripperExtensionCfg
 from .g1_cfg import G1Dof12Cfg, G1Dof23Cfg, G1Dof27Cfg, G1Dof29Cfg, G1Dof29Dex3Cfg, G1Dof29SensorsCfg
 from .gen3_cfg import Gen3Cfg
+from .go2_cfg import Go2Cfg
 from .google_robot_static_cfg import GoogleRobotStaticCfg
 from .h1_2_without_hand_cfg import H12WithoutHandCfg
 from .h1_body_collision import H1BodyCollisionCfg
