77c576a23491a3c8ba3d17a5832bdeae262982a3
myuansun <1273414643@qq.com>
2025-11-28 21:42:16 +0800

    [update] align batch size

--- roboverse_learn/rl/clean_rl/ppo.py
+++ roboverse_learn/rl/clean_rl/ppo.py
@@ -77,7 +77,7 @@ class Args:
     """total timesteps of the experiments"""
     learning_rate: float = 3e-4
     """the learning rate of the optimizer"""
-    num_envs: int = 1024
+    num_envs: int = 4096
     """the number of parallel game environments"""
     num_steps: int = 64
     """the number of steps to run in each environment per policy rollout"""
